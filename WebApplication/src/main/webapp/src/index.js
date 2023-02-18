72e6fa7bc72eee01735436b99c13d427da6e27da
19130004 <19130004@st.hcmuaf.edu.vn>
2023-02-18 16:20:33 +0700

    push reactjs base

--- WebApplication/src/main/webapp/src/index.js
+++ WebApplication/src/main/webapp/src/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./sass/index.scss";
+//pages
+import { Home } from "./pages";
+//components
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { ToastContainer } from "react-toastify";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="container">
+          <div className="main">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+      <ToastContainer />
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ WebApplication/src/main/webapp/src/redux/store.js
@@ -0,0 +1,5 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+const rootReducer = combineReducers({});
+export const store = configureStore({
+  reducer: rootReducer,
+});
