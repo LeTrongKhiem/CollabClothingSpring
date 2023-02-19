9bd8d16847043803f8ae6f72769ecf6b9dcd870b
19130004 <19130004@st.hcmuaf.edu.vn>
2023-02-19 20:40:15 +0700

    fix crossOrigin

--- WebApplication/src/main/webapp/src/index.js
+++ WebApplication/src/main/webapp/src/index.js
@@ -2,31 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./sass/index.scss";
 //pages
-import { Home } from "./pages";
+import {Home, Register} from "./pages";
 //components
 
-import { ToastContainer } from "react-toastify";
+import {ToastContainer} from "react-toastify";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <div className="container">
-          <div className="main">
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
-          </div>
-        </div>
-      </BrowserRouter>
-      <ToastContainer />
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <div className="container">
+                    <div className="main">
+                        <Routes>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="/register" element={<Register/>}/>
+                        </Routes>
+                    </div>
+                </div>
+            </BrowserRouter>
+            <ToastContainer/>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
