332aba38de1a31277e601687d1f1fcbe751e6a1d
19130004 <19130004@st.hcmuaf.edu.vn>
2023-02-28 16:46:39 +0700

    hot fix url

--- WebApplication/src/main/webapp/src/services/UserService.js
+++ WebApplication/src/main/webapp/src/services/UserService.js
@@ -4,27 +4,27 @@ import axios from "axios";
 axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
 axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
 axios.defaults.headers.post["Content-Type"] = "application/json";
-const API_URL = "http://localhost:6666/api/auth";
+const API_URL = "http://localhost:6868/api/auth";
 
 class UserService {
-  saveUser(user) {
-    return axios.post("http://localhost:6868/api/auth/register", user);
-  }
+    saveUser(user) {
+        return axios.post(API_URL + "/register", user);
+    }
 
-  verifyEmail(token)   {
-    return axios.get(
-        `http://localhost:6868/api/auth/verify?code=${token}`)
-  }
+    verifyEmail(token) {
+        return axios.get(
+            API_URL + `/verify?code=${token}`)
+    }
 
-  checkEmailExist(email) {
-    return axios.get(
-      "http://localhost:6868/api/auth/checkEmailExist?email=" + email
-    );
-  }
+    checkEmailExist(email) {
+        return axios.get(
+            API_URL + "/checkEmailExist?email=" + email
+        );
+    }
 
-  login(user) {
-    return axios.post("http://localhost:6868/api/auth/login", user);
-  }
+    login(user) {
+        return axios.post(API_URL+"/login", user);
+    }
 }
 
 export default new UserService();
