1c75e2311ed1d22ded524eeeabb2e506d9a8f51b
19130004 <19130004@st.hcmuaf.edu.vn>
2023-03-08 16:09:31 +0700

    login page

---
+++ WebApplication/src/main/admin/src/services/UserService.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
+
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+const API_URL = 'http://localhost:6868/api/auth';
+const API_URL_USER = 'http://localhost:6868/api/users';
+class UserService {
+
+    login(user) {
+        return axios.post(API_URL + "/authenticate", user);
+    }
+    getCurrentUser() {
+        const token = localStorage.getItem("token");
+        return axios.get(API_URL_USER+"/userProfile", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+    }
+}
+
+export default new UserService();
