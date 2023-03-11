cb21e0589f95ba6ac6e4b3fa6002fce854bb1979
LeTrongKhiem <19130102@st.hcmuaf.edu.vn>
2023-03-11 19:37:32 +0700

    Pagination Get all Product and Product Images

--- CollabClothing.API/src/main/java/com/ecommerce/Application/Mappings/ProductImageMapping.java
+++ CollabClothing.API/src/main/java/com/ecommerce/Application/Mappings/ProductImageMapping.java
@@ -1,10 +1,14 @@
 package com.ecommerce.Application.Mappings;
 
+import com.ecommerce.Application.Extensions.DateTimeExtensions;
 import com.ecommerce.Entities.Product;
 import com.ecommerce.Entities.ProductImage;
+import com.ecommerce.Model.Products.ImageModel;
 import com.ecommerce.Model.Products.ProductImageModel;
 
+import java.sql.Timestamp;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public class ProductImageMapping {
@@ -12,7 +16,7 @@ public class ProductImageMapping {
         ProductImage productImage = new ProductImage();
         UUID id = UUID.randomUUID();
         productImage.setId(id);
-        productImage.setAlt(model.getAlt());
+        productImage.setAlt(product.getName());
         productImage.setThumbnail(model.isThumbnail());
         productImage.setProduct(product);
         productImage.setCreatedBy(createBy);
@@ -20,5 +24,24 @@ public class ProductImageMapping {
         productImage.setIsDeleted(false);
         productImage.setPath(path);
         return productImage;
+    }
+
+    public static ImageModel mapToImageModel(ProductImage productImage) {
+        ImageModel imageModel = new ImageModel();
+        imageModel.setId(productImage.getId());
+        imageModel.setAlt(productImage.getAlt());
+        imageModel.setCreatedDate(productImage.getCreatedDate());
+        imageModel.setCreatedBy(productImage.getCreatedBy());
+        imageModel.setFileName(productImage.getPath());
+        imageModel.setDeleted(productImage.getIsDeleted());
+        imageModel.setThumbnail(productImage.isThumbnail());
+        imageModel.setModifiedBy(productImage.getModifiedBy());
+        imageModel.setModifiedDate(productImage.getModifiedDate());
+        imageModel.setUrl(productImage.getPath());
+        return imageModel;
+    }
+
+    public static List<ImageModel> mapToImageModel(List<ProductImage> productImages) {
+        return productImages.stream().map(ProductImageMapping::mapToImageModel).toList();
     }
 }
--- CollabClothing.API/src/main/java/com/ecommerce/Model/PagingModel.java
+++ CollabClothing.API/src/main/java/com/ecommerce/Model/PagingModel.java
@@ -1,13 +1,20 @@
 package com.ecommerce.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PagingModel<T> {
     public List<T> results;
-    public int ResultCount;
     public int PageSize;
     public int TotalCount;
     public int CurrentPage;
     public int TotalPages;
-    public String Search;
 }
