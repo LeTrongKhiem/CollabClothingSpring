bfe256c1f884db49eef3e93a9a3fb5070b39000f
LeTrongKhiem <19130102@st.hcmuaf.edu.vn>
2023-03-22 16:50:47 +0700

    hotfixes: add import library

--- Infrastructure/src/main/java/com/ecommerce/Entities/ProductDetail.java
+++ Infrastructure/src/main/java/com/ecommerce/Entities/ProductDetail.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import java.io.Serial;
 import java.io.Serializable;
