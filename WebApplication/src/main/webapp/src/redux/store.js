6b9c8b0454c6a4cb1a046e75735dcf9d1a5438d8
19130004 <19130004@st.hcmuaf.edu.vn>
2023-05-17 15:23:20 +0700

    sample home page

--- WebApplication/src/main/webapp/src/redux/store.js
+++ WebApplication/src/main/webapp/src/redux/store.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slice/authSlice";
 import productModalReducer from "./slice/productModalSlice";
+import cartItemsReducer from "./slice/cartItemsSlice"
 const rootReducer = combineReducers({
   // Add your reducers here
   auth: authReducer,
   productModal: productModalReducer,
+  cartItems: cartItemsReducer
 
 });
 export const store = configureStore({
