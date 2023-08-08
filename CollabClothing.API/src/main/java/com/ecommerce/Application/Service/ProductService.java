ec29b6e5a5ed2342b78c946ddfbb2c027ffbc1cf
Trần Lê Anh Khoa <19130108@st.hcmuaf.edu.vn>
2023-08-08 15:22:50 +0700

    review code

--- CollabClothing.API/src/main/java/com/ecommerce/Application/Service/ProductService.java
+++ CollabClothing.API/src/main/java/com/ecommerce/Application/Service/ProductService.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
+
 import org.webjars.NotFoundException;
 
 import java.io.File;
